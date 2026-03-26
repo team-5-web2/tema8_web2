e6bcae7b8beedaf4719710fa73b882e2cfaef602
mipe0018 <mipe0018@stud.ek.dk>
2026-03-26 11:34:14 +0100

    rettelser i javascript "alle produkter"

--- Javascript/allekategorier.js
+++ Javascript/allekategorier.js
@@ -6,7 +6,7 @@ const categories = ["furniture", "home-decoration", "kitchen-accessories"];
 
 productContainer.innerHTML = "";
 
-categories.forEach((category) => {
+/*ategories.forEach((category) => {
   fetch(`https://dummyjson.com/products/category/${category}`)
     .then((res) => res.json())
     .then((data) => {
@@ -24,4 +24,28 @@ categories.forEach((category) => {
         `;
       });
     });
-});
+});*/
+fetchProducts(categories, 0);
+function fetchProducts(categoryArr, index) {
+  fetch(`https://dummyjson.com/products/category/${categoryArr[index]}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.products.forEach((product) => {
+        productContainer.innerHTML += `
+          <div class="cardSoldout"><span class="badge">-${product.discountPercentage}%</span>
+            <a href="produkt.html?id=${product.id}">
+              <img src="${product.thumbnail}" alt="${product.title}">
+            </a>
+            <p>${product.title}</p>  <p>Førpris:</p>
+          <span class="oldprice">${Math.round(product.price / (1 - product.discountPercentage / 100))},- DKK</span><br />
+          <span class="newprice">Nypris: ${product.price},- DKK</span>
+          <button class="buybutton">Læg i kurv</button>
+          </div>
+        `;
+      });
+      index++;
+      if (index < categoryArr.length) {
+        fetchProducts(categoryArr, index);
+      }
+    });
+}
